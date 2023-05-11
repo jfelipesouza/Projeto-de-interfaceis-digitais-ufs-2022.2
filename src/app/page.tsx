3e9cc2d4946933012dca68f616eda447194b8765
jfelipesouza <joao.felipe.18.dev@gmail.com>
2023-05-11 16:55:30 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,23 +12,26 @@ export default function Home() {
     <>
       <Header />
       <main className="flex min-h-screen flex-col">
-        <section className="flex max-w-screen min-h-screen" id="hero">
-          <div className="flex flex-1 flex-col gap-8 justify-center pl-20">
-            <h2 className="sm:text-3xl md:text-4xl lg:text-6xl text-white font-black">
+        <section
+          className="flex max-w-screen min-h-banner sm:min-h-screen"
+          id="hero"
+        >
+          <div className="flex flex-1 flex-col gap-8 justify-center pl-8 sm:pl-20">
+            <h2 className="text-4xl sm:text-3xl md:text-4xl lg:text-6xl text-white font-black">
               Design e Tecnologia G
             </h2>
-            <h3 className="sm:text-2xl md:text-2xl lg:text-4xl text-white font-black">
+            <h3 className="text-2xl text-white font-black">
               Interfaces Digitais | Turma 01
             </h3>
           </div>
-          <div className="flex flex-1 justify-center items-center flex-col bg-contrast rounded-tl-full rounded-bt-full ">
+          <div className=" hidden lg:flex flex-1 justify-center items-center flex-col bg-contrast rounded-tl-full rounded-bt-full ">
             <div className="w-full h-full relative mr-20 ">
               <Image src={'assets/images/banner2.svg'} alt="banner" fill />
             </div>
           </div>
         </section>
         <section
-          className="flex flex-col max-w-screen min-h-screen px-20 pt-20 gap-8"
+          className="flex flex-col max-w-screen min-h-banner px-7 pb-8 sm:min-h-screen sm:px-8 lg:px-20 lg:pt-20 gap-8"
           id="activities"
         >
           <h2 className={'text-white font-black text-5xl mb-8'}>Atividades</h2>
